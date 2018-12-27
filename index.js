da5808c6921eca8cce94e45c410eb05c390c158b
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2018-12-26 19:57:39 -0800

    stop tracking now.json, small updates

--- index.js
+++ index.js
@@ -10,10 +10,6 @@ app.use(cors())
 
 const PORT = 3000
 
-app.listen(PORT, () => {
-  console.log(`App is running on port: ${PORT}`)
-})
-
 const T = new Twit({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
@@ -22,6 +18,7 @@ const T = new Twit({
 
 app.get('/', (req, res) => {
   res.send('SCD Guest Twitter Photos')
+  res.end()
 })
 
 app.get('/lookup', (req, res) => {
@@ -30,19 +27,24 @@ app.get('/lookup', (req, res) => {
     res.json({
       error: 'Gotta have some screen_names in a query param to lookup!'
     })
-    return
+    res.end()
   }
 
   T.get(
     'users/lookup',
     { screen_name: req.query.screen_name.split(',') },
     (err, data, response) => {
+      if (err) {
+        throw new Error(err)
+      }
+
       const slimData = data.map(({ profile_image_url_https, screen_name }) => ({
         profile_image_url_https,
         screen_name
       }))
 
       res.json(slimData)
+      res.end()
     }
   )
 })
@@ -54,14 +56,23 @@ app.get('/show', (req, res) => {
       error:
         'Gotta have a screen_name in a query param to look up the user data!'
     })
-    return
+    res.end()
   }
 
   T.get(
     'users/show',
     { screen_name: req.query.screen_name },
     (err, data, response) => {
+      if (err) {
+        throw new Error(err)
+      }
+
       res.json(data)
+      res.end()
     }
   )
+})
+
+app.listen(PORT, () => {
+  console.log(`App is running on port: ${PORT}`)
 })
